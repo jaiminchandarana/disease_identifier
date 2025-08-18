ece5d802d2e72974fd2d2d529db38af35ed88305
Jaimin Chandarana <137302744+jaiminchandarana@users.noreply.github.com>
2025-08-18 09:19:58 +0530

    Update main.py

--- diagnosis/src/diagnosis/main.py
+++ diagnosis/src/diagnosis/main.py
@@ -4,10 +4,34 @@ from diagnosis.extract import extract_text_from_image,extract_text_from_pdf
 from diagnosis.crew import Diagnosis
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")        
 
-# content = extract_text_from_image(r"Path to your image")   --> Keep this if you want to work with image.
-content = extract_text_from_pdf(r"Path to your pdf")  # Keep this if you want to work with pdf.
+def selection():
+    try:
+        print("1.Image\n2.PDF")
+        choice = input("Enter your choice : ").lower()
+        if choice == "1" or choice == "image":
+            path = input("Enter path to your image : ")
+            if path.lower().endswith(".png") or path.lower().endswith(".jpg"):
+                content = extract_text_from_image(path)
+            else:
+                print("Enter a legit path to your image.")
+        elif choice == "2" or choice == "pdf":
+            path = input("Enter path to your pdf : ")
+            if path.lower().endswith(".pdf"):
+                content = extract_text_from_pdf(path)
+            else:
+                print("Enter a legit path to your pdf.")
+        else:
+            print("select a legit option.(1 or 2).")
+            return selection()
+        return content
+    except Exception as e:
+        print("Error occured, try again with legit choice and path.")
+        return selection()
+    
+content = selection()
 mid = len(content) // 2
 content1, content2 = content[:mid], content[mid:]
+
 def run():
     """
     Run the crew.
